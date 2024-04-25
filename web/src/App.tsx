de6c46de5a2580fe1f2d0becc116597d0f5e1a39
cristianchaparroa <cristianchaparroa@gmail.com>
2024-04-24 21:09:24 -0500

    add wallet connect button using rainbow kit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {getDefaultConfig, RainbowKitProvider} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {scrollSepolia} from 'wagmi/chains';
+import {QueryClientProvider, QueryClient,} from "@tanstack/react-query";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-function App() {
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Pocket!
-    </h1>
-  )
-}
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+    appName: 'pocket',
+    projectId: '71d8c3310f0fb3c0788d5a4b6582820e',
+    chains: [scrollSepolia],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const App = () => {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    Pocket !!
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            padding: 12,
+                        }}
+                    >
+                        <ConnectButton/>
+                    </div>
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+);
+};
 
 export default App
