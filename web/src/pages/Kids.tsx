83bf354d8b87018420fc10bb64f045cefbbe7e7f
cristianchaparroa <cristianchaparroa@gmail.com>
2024-04-28 21:47:41 -0500

    try to fix dep

--- web/src/pages/Kids.tsx
+++ web/src/pages/Kids.tsx
@@ -1,8 +1,16 @@
 import defaultIcon from '/person-circle-icon.png';
 import Navbar from '../components/Navbar';
 import { menuProps } from './constants';
+import {useAccount} from "wagmi";
+import PocketService from "../services/contracts/PocketService.ts";
 
 const KidsPage = () => {
+    /** Wallet account logged */
+    const {isConnected, address} = useAccount();
+
+    /** Service used to interact with pocket contract */
+    const pocketService  = new PocketService(address);
+
 
     const kids = [
       {"name":"James", "ammount":"200k", "image":""} ,
@@ -17,7 +25,7 @@ const KidsPage = () => {
                     kids.map( (kid, index) => (
                         <div key={index} className="flex flex-row max-w-xl p-5">
                             <img className="w-8 h-8 mr-5" src={defaultIcon} />
-                            {kid.name} - {kid.ammount} 
+                            {kid.name} - {kid.ammount}
                         </div>
                     ))
                 }
