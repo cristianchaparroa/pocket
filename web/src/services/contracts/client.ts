a9dd3578d733aa2e65e43e414d1878c125c23391
Cristian <cristianchaparroa@users.noreply.github.com>
2024-04-28 22:50:15 -0400

    try to fix dep (#8)

--- web/src/services/contracts/client.ts
+++ web/src/services/contracts/client.ts
@@ -1,21 +1,7 @@
-import { createPublicClient, createWalletClient, http, custom } from 'viem'
+import { createPublicClient, http } from 'viem'
 import { scrollSepolia } from 'viem/chains'
-import { EthereumProvider } from '@walletconnect/ethereum-provider'
-import WalletConnectProjectID from "./constants.ts";
 
 export const publicClient = createPublicClient({
     chain: scrollSepolia,
     transport: http(),
-})
-
-// eg: WalletConnect
-const provider = await EthereumProvider.init({
-    projectId: WalletConnectProjectID,
-    showQrModal: true,
-    chains: [1],
-})
-
-export const walletClientWC = createWalletClient({
-    chain: scrollSepolia,
-    transport: custom(provider),
 })
